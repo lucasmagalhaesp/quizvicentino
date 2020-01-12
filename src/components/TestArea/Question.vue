0b4331171e2ad0cbd852796e318c894a36638dc1
Lucas Magalhães <lucasmagalhaes1909@gmail.com>
2020-01-12 10:15:08 -0300

    Modificado a forma de busca das perguntas para a organização do teste. Antes todas as perguntas eram buscadas no início, agora elas são buscadas separadamente, para que o usuário não tenha acesso prévio via navegador

--- src/components/TestArea/Question.vue
+++ src/components/TestArea/Question.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <h2>Pergunta {{ numQuestion }}</h2>
-        <h6>{{ questions[numQuestion - 1].question.description }}</h6>
+        <h6>{{ currentQuestion.question.description }}</h6>
         <b-form-group>
-            <b-form-radio v-for="(answer, index) in questions[numQuestion - 1].answers" :key="index" v-model="userResponse" name="answer" :value="answer.id" :disabled="disableAnswers">{{ answer.description }}</b-form-radio>
+            <b-form-radio v-for="(answer, index) in currentQuestion.answers" :key="index" v-model="userResponse" name="answer" :value="answer.id" :disabled="disableAnswers">{{ answer.description }}</b-form-radio>
         </b-form-group>
         <b-button-group>
             <b-button variant="success" @click="getCorrectResponse">Confirmar</b-button>
@@ -18,25 +18,29 @@
 export default {
     data(){
         return {
-            questions: [],
-            numQuestion: 1,
+            currentQuestion: [],
+            numQuestion: 0,
             userResponse: 0,
             disableAnswers: false,
+            idQuestions: []
         }
     },
     created(){
-        axios("tests/createTest").then(response => {
-            response =  response.data;
-            if (response.success){
-                this.questions = response.data;
-                this.$store.commit("startTimer");
-            } 
-        });
+        this.numQuestion = 1;
     },
     methods:{
+        getQuestion(){
+            axios.post(`tests/selectTestQuestion`, { idQuestions: this.idQuestions } ).then(response => {
+                response =  response.data;
+                if (response.success){
+                    this.currentQuestion = response.data;
+                    this.idQuestions.push(response.indexQuestion);
+                } 
+            });
+        },
         getCorrectResponse(){
             this.$store.commit("stopTimer");
-            let idQuestion = this.questions[this.numQuestion - 1].question.id;
+            let idQuestion = this.currentQuestion.question.id;
             axios(`tests/correctAnswer/${idQuestion}`).then(response => {
                 response =  response.data;
                 if (response.success){
@@ -59,6 +63,8 @@ export default {
             if (value > 5){
                 console.log('resultado');debugger;
             }
+
+            this.getQuestion();
             this.$store.commit("startTimer");
             this.disableAnswers = false;
         }
