37ba12903a641660de469f58798cf7d37dd1f881
Lucas Magalhães <lucasmagalhaes.dev@gmail.com>
2023-03-23 16:43:39 -0300

    - Alteração nos botões e no slider da página inicial

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,33 +5,36 @@
       mode="out-in" appear
     >
       <div>
-        <div class="row">
+        <div class="row" v-if="!$q.screen.lt.sm">
           <div class="col-12">
             <carousel />
           </div>
         </div>
-        <div class="row">
-          <div class="col-12 col-md-3 q-pr-md">
-            <card-index img="https://quizvicentino.com.br/images/index/quiz.png" linkBtn="/about" labelBtn="Sobre">
-              
-            </card-index>
+        <section class="q-my-lg">
+          <div class="row justify-center">
+            <div class="col-8 col-md-3 q-px-md">
+              <card-index img="images/index/quiz.png" linkBtn="/sobre" labelBtn="Sobre" />
+            </div>
+            <div class="col-8 col-md-3 q-px-md" v-if="!logged">
+              <card-index img="images/index/registro.png" linkBtn="/cadastrar" labelBtn="Cadastre-se" />
+            </div>
+            <div class="col-8 col-md-3 q-px-md" v-if="!logged">
+              <card-index img="images/index/login.png" linkBtn="/login" labelBtn="Login" />
+            </div>
+            <div class="col-8 col-md-3 q-px-md">
+              <card-index img="images/index/jogar.png" linkBtn="/jogar/instrucoes" labelBtn="Jogar" />
+            </div>
+            <div class="col-8 col-md-3 q-px-md" v-if="logged">
+              <card-index img="images/index/testes.png" linkBtn="/testes" labelBtn="Meus Testes" />
+            </div>
+            <div class="col-8 col-md-3 q-px-md" v-if="logged">
+              <card-index img="images/index/ranking.png" linkBtn="/ranking" labelBtn="Ranking" />
+            </div>
           </div>
-          <div class="col-12 col-md-3 q-pr-md">
-            <card-index img="https://quizvicentino.com.br/images/index/registro.png" linkBtn="/register" labelBtn="Cadastre-se">
-              
-            </card-index>
-          </div>
-          <div class="col-12 col-md-3 q-pr-md">
-            <card-index img="https://quizvicentino.com.br/images/index/login.png" linkBtn="/login" labelBtn="Login">
-              
-            </card-index>
-          </div>
-          <div class="col-12 col-md-3 q-pr-md">
-            <card-index img="https://quizvicentino.com.br/images/index/jogar.png" linkBtn="/test-area" labelBtn="Jogar">
-             
-            </card-index>
-          </div>
-        </div>
+        </section>
+        <!-- <section style="padding: 0" class="q-mb-lg">
+            <ssvp />
+        </section> -->
       </div>  
     </transition>
   </q-page>
@@ -40,14 +43,28 @@
 <script>
 import CardIndex from '../components/Custom/CardIndex';
 import Carousel from '../components/Home/Carousel';
+import Ssvp from '../components/Home/SSVP';
 export default {
     components:{
       CardIndex,
-      Carousel
+      Carousel,
+      Ssvp
     },
-  name: 'PageIndex',
-  data(){
-    return { logo: 'statics/images/logo.png'}
-  }
+    data(){
+        return { logo: 'statics/images/logo.png'}
+    },
+    computed:{
+        logged(){
+            return this.$store.getters.logged
+        }
+    }
 }
 </script>
+<style scoped>
+  @media(min-width: 599px){
+    section{
+      padding: 0 50px
+    }
+  }
+  
+</style>
